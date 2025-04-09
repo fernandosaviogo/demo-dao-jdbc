fa3494da15053adf8d28f2ec7d650ddbf1936a85
Fernando Sávio Gomes <133050259+fernandosaviogo@users.noreply.github.com>
2025-04-09 14:11:39 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Seller;
+
+public interface SellerDao {
+	
+	void insert(Seller obj);  // Operação que insere no banco de dados
+	void update(Seller obj);  // Operação que atualiza o Banco de dados
+	void deleteById(Integer id);  // Operação que deleta uma linha do banco conforme o id informado
+	Seller findId(Integer id);  // Opeção que consulta um ojeto no banco de dados e retorna False ou True
+	List<Seller> findAll();  // Operação que retorna todos os departamentos
+
+}
