61997372baea8e5c8aff2138bfbbb0fb9ed370fc
Fernando Sávio Gomes <133050259+fernandosaviogo@users.noreply.github.com>
2025-04-10 14:17:04 -0300

    Reusing instantiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -52,17 +52,8 @@ public class SellerDaoJDBC implements SellerDao{
 			st.setInt(1, id);            
 			rs = st.executeQuery();
 			if(rs.next()) {
-				Department dep = new Department();  // implementação do objeto Department direto da classe
-				
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
-				Seller obj = new Seller();
-				obj.setId(rs.getInt("Id"));
-				obj.setName(rs.getString("Name"));
-				obj.setEmail(rs.getString("Email"));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setBirthDate(rs.getDate("BirthDate"));
-				obj.setDepartment(dep);  // Usa o objeto criado da classe Department para trazer os dados do departamento montados
+				Department dep = instantiateDepartment(rs);
+				Seller obj = instantiateSeller(rs, dep);
 				return obj;
 			}
 			return null;
@@ -74,7 +65,26 @@ public class SellerDaoJDBC implements SellerDao{
 		}
 		
 	}
-		
+	
+	// Metodo auxiliar Seller
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);  // Usa o objeto criado da classe Department para trazer os dados do departamento montados
+		return obj;
+	}
+
+	// Metodo auxiliar Department
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();  // implementação do objeto Department direto da classe
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
+	}
 
 	@Override
 	public List<Seller> findAll() {
