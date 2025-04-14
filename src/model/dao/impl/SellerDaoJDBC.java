4d8574dd38e82de772b17722f78502e5977a9f7d
Fernando Sávio Gomes <133050259+fernandosaviogo@users.noreply.github.com>
2025-04-14 08:38:40 -0300

    findAll implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -88,13 +88,46 @@ public class SellerDaoJDBC implements SellerDao{
 		dep.setName(rs.getString("DepName"));
 		return dep;
 	}
-
+	
+	// Busca todos os vendedores por nome de departamento
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id " 
+					+ "ORDER BY Name");
+			
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>(); // Map criado para guardar os departamentos instaciados
+			
+			while (rs.next()) {
+				
+				Department dep = map.get(rs.getInt("DepartmentId"));  // Verifica se existe o departamento ja instaciado... se exirtir retorna nullo
+				
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+								
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
-
+	
+	// busca os vendedores conforme numero do departeamento digitado
 	@Override
 	public List<Seller> findByDepartment(Department department) {
 		PreparedStatement st = null;
